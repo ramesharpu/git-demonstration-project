a3d46fb0b790826e8c6c7dc67d6f95be4c179722
Ramesh A <ramesharpu@gmail.com>
2025-09-13 07:35:42 +0530

    added new file and modified the existing file

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -5,6 +5,11 @@ public class HelloWorld {
 	public void welcomeMsg() {
 		System.out.println("Welcome to Java Programming!");
 	}
+	
+	//from b1 branch
+	public void displayInfo() {
+		System.out.println("This is a simple Java program.");
+	}
 
 	public static void main(String[] args) {
 		HelloWorld obj = new HelloWorld();
